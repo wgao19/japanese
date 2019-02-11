91e779e94744c92e0a5d0c068e2e2dc7d34e646f
Wei Gao <wgao19@gmail.com>
2019-02-11 17:46:57 +0800

    Update tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-134242847-1`,
       },
     },
     `gatsby-plugin-feed`,
